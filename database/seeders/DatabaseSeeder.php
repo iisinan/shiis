d1a296b554ee4d80308f800fb2fc75d026ad4411
ismailasinan-cmyk <ismailasinan@gmail.com>
2026-05-06 11:20:46 +0100

    Add default agenda and administrative account seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,9 +41,9 @@ class DatabaseSeeder extends Seeder
         $admin->assignRole('Super Admin');
 
         $accountant = User::firstOrCreate(
-            ['email' => 'accountant@shiis2005.com'],
+            ['email' => 'finance@shiis05.com'],
             [
-                'name' => 'Main Accountant',
+                'name' => 'Reunion Finance Team',
                 'password' => bcrypt('password'),
                 'is_paid' => true,
                 'is_active' => true,
@@ -51,5 +51,55 @@ class DatabaseSeeder extends Seeder
             ]
         );
         $accountant->assignRole('Accountant');
+
+        // Default Agenda
+        $agendaItems = [
+            [
+                'time' => '09:00 AM',
+                'title' => 'Arrival & Registration',
+                'description' => 'Welcome home! Collect your reunion kit, name tag, and commemorative souvenir at the entrance.',
+                'location' => 'Main Reception Hall',
+                'order' => 1
+            ],
+            [
+                'time' => '10:00 AM',
+                'title' => 'Opening Ceremony',
+                'description' => 'Official kick-off of the SHIIS 05 Reunion. Welcome address by the organizing committee and special guest speakers.',
+                'location' => 'Grand Ballroom',
+                'order' => 2
+            ],
+            [
+                'time' => '11:30 AM',
+                'title' => 'Networking & Refreshments',
+                'description' => 'Connect with old friends over light snacks and drinks. A great time to catch up on the last two decades.',
+                'location' => 'Garden Terrace',
+                'order' => 3
+            ],
+            [
+                'time' => '01:00 PM',
+                'title' => 'The Journey So Far (Panel)',
+                'description' => 'An inspiring session featuring alumni sharing their career paths, life lessons, and success stories.',
+                'location' => 'Auditorium',
+                'order' => 4
+            ],
+            [
+                'time' => '03:00 PM',
+                'title' => 'Gala Dinner & Awards',
+                'description' => 'A premium 3-course dinner followed by special recognition awards for outstanding alumni.',
+                'location' => 'Main Banquet Hall',
+                'order' => 5
+            ],
+            [
+                'time' => '06:00 PM',
+                'title' => 'Closing Remarks & Photos',
+                'description' => 'Final group photography and official closing of the reunion festivities.',
+                'location' => 'Front Courtyard',
+                'order' => 6
+            ]
+        ];
+
+        foreach ($agendaItems as $item) {
+            \App\Models\Agenda::firstOrCreate(['title' => $item['title']], $item);
+        }
     }
 }
